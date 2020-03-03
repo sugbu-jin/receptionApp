dc659032ecf60f3edf52f162636918427cda983b
c0mpumast3r <frank.semilla@gmail.com>
2020-03-03 19:43:57 +0800

    Updates

---
+++ precache-manifest.de264283154e2203d7cc33847e248a3d.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9979b2f352a6af52334620d4589c43",
+    "url": "/receptionapp/index.html"
+  },
+  {
+    "revision": "3eaf0d86eeabb9e9d349",
+    "url": "/receptionapp/static/css/2.903a27b1.chunk.css"
+  },
+  {
+    "revision": "ed9299ca63851b693b2b",
+    "url": "/receptionapp/static/css/main.75bc5d6e.chunk.css"
+  },
+  {
+    "revision": "3eaf0d86eeabb9e9d349",
+    "url": "/receptionapp/static/js/2.83e13fec.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/receptionapp/static/js/2.83e13fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9299ca63851b693b2b",
+    "url": "/receptionapp/static/js/main.2cb408b2.chunk.js"
+  },
+  {
+    "revision": "c911bc2843645002493a",
+    "url": "/receptionapp/static/js/runtime-main.b8b404ae.js"
+  },
+  {
+    "revision": "f21658b1b5b879da1436bf3e2d0afa3d",
+    "url": "/receptionapp/static/media/btn-ico-conf-normal.f21658b1.png"
+  },
+  {
+    "revision": "6bef40b13c33048f597a15ade8ba5c9b",
+    "url": "/receptionapp/static/media/btn-ico-conf-selected.6bef40b1.png"
+  },
+  {
+    "revision": "a9f75eff37988077e19f376820d03fcf",
+    "url": "/receptionapp/static/media/btn-ico-daily-normal.a9f75eff.png"
+  },
+  {
+    "revision": "baf9602a6d8643567fceb5f47bb30c96",
+    "url": "/receptionapp/static/media/btn-ico-daily-selected.baf9602a.png"
+  },
+  {
+    "revision": "70a8dd95ddd1873d4f55f475f79d7999",
+    "url": "/receptionapp/static/media/btn-ico-hourly-normal.70a8dd95.png"
+  },
+  {
+    "revision": "c8033901b939410599662b5e163a3b7b",
+    "url": "/receptionapp/static/media/btn-ico-hourly-selected.c8033901.png"
+  },
+  {
+    "revision": "7b90176ef22d08b9bbbc1eb2a62c3bc1",
+    "url": "/receptionapp/static/media/btn-ico-monthly-normal.7b90176e.png"
+  },
+  {
+    "revision": "96dcf3aa1d5b5ac02ada20f7d026ccbc",
+    "url": "/receptionapp/static/media/btn-ico-monthly-selected.96dcf3aa.png"
+  },
+  {
+    "revision": "1c97cded22556824ae9756ca9ee78b88",
+    "url": "/receptionapp/static/media/btn-ico-rating-1-active.1c97cded.png"
+  },
+  {
+    "revision": "9d9c986a60b490e72c0e9ba96bab66ec",
+    "url": "/receptionapp/static/media/btn-ico-rating-1-selected.9d9c986a.png"
+  },
+  {
+    "revision": "adbb4af2b5187cae4b2a4d1c200f5629",
+    "url": "/receptionapp/static/media/btn-ico-rating-2-active.adbb4af2.png"
+  },
+  {
+    "revision": "b254d0a7d752b0f2b9f3fcd24e5267ad",
+    "url": "/receptionapp/static/media/btn-ico-rating-2-selected.b254d0a7.png"
+  },
+  {
+    "revision": "483fca924747d56ffb48cdbaf7b60946",
+    "url": "/receptionapp/static/media/btn-ico-rating-3-active.483fca92.png"
+  },
+  {
+    "revision": "965a5ed39c3e38df5ce6820bea3aa757",
+    "url": "/receptionapp/static/media/btn-ico-rating-4-active.965a5ed3.png"
+  },
+  {
+    "revision": "724f61c4df8f56cf373e51590b407e7d",
+    "url": "/receptionapp/static/media/btn-ico-rating-4-selected.724f61c4.png"
+  },
+  {
+    "revision": "18f5fa00e6f4051460466ce924d67f0d",
+    "url": "/receptionapp/static/media/btn-ico-rating-5-active.18f5fa00.png"
+  },
+  {
+    "revision": "c22ada8626491000d11b711544e918e8",
+    "url": "/receptionapp/static/media/btn-ico-rating-5-selected.c22ada86.png"
+  },
+  {
+    "revision": "e7be905d690878ff46eb50cddcafb04d",
+    "url": "/receptionapp/static/media/btn-setting.e7be905d.png"
+  },
+  {
+    "revision": "d130a843d5d791cb6d45bcf14d0a6753",
+    "url": "/receptionapp/static/media/conferenceselicon.d130a843.png"
+  },
+  {
+    "revision": "0a5d591bc1be0ef78bd7a512d177e70e",
+    "url": "/receptionapp/static/media/conferencetitle.0a5d591b.png"
+  },
+  {
+    "revision": "6b3688dd4cbb2ab5a60361af0f5474cb",
+    "url": "/receptionapp/static/media/drinkselectionselicon.6b3688dd.png"
+  },
+  {
+    "revision": "8b0323f353ac24c7d1f10aafd0e63ab1",
+    "url": "/receptionapp/static/media/drinkselectiontitle.8b0323f3.png"
+  },
+  {
+    "revision": "4be588796c3f4ff587b9ff763fb2fcc5",
+    "url": "/receptionapp/static/media/homeicon.4be58879.png"
+  },
+  {
+    "revision": "95e2fed310a719d1bfe99f92af0da9cb",
+    "url": "/receptionapp/static/media/ico-brewed-normal.95e2fed3.png"
+  },
+  {
+    "revision": "7e9135689d4c6e40942f4cedb03ed1e3",
+    "url": "/receptionapp/static/media/ico-brewed-selected.7e913568.png"
+  },
+  {
+    "revision": "a840897ce6b92f30a0e6196456215ae0",
+    "url": "/receptionapp/static/media/ico-iced-normal.a840897c.png"
+  },
+  {
+    "revision": "49211afcf75d2b5018417445c0f931de",
+    "url": "/receptionapp/static/media/ico-iced-selected.49211afc.png"
+  },
+  {
+    "revision": "3d2550525c621593344e359946286e19",
+    "url": "/receptionapp/static/media/ico-other-normal.3d255052.png"
+  },
+  {
+    "revision": "43ff38012dbb9184ec7f1360c5768f69",
+    "url": "/receptionapp/static/media/ico-other-selected.43ff3801.png"
+  },
+  {
+    "revision": "9308114bba4546ac2fd12bc17c6b1809",
+    "url": "/receptionapp/static/media/img-conference-notitle.9308114b.png"
+  },
+  {
+    "revision": "00707a2fa430be9fe47a6d5d3d61c65c",
+    "url": "/receptionapp/static/media/img-homepage-bg.00707a2f.png"
+  },
+  {
+    "revision": "5d5efd1baf63e661e7c27f489e8b88e3",
+    "url": "/receptionapp/static/media/img-lastscreen.5d5efd1b.png"
+  },
+  {
+    "revision": "31ee7cba26a9fcdb80de5d44ca688c99",
+    "url": "/receptionapp/static/media/img-rating.31ee7cba.png"
+  },
+  {
+    "revision": "d97940a675824b4ae2ecc92d763ec847",
+    "url": "/receptionapp/static/media/personalinfoicon.d97940a6.png"
+  },
+  {
+    "revision": "8eb09b4614fd1f0cb77e2e35d4d305b0",
+    "url": "/receptionapp/static/media/personalinfoselicon.8eb09b46.png"
+  },
+  {
+    "revision": "085d4793167605ec013ca49ae9098f04",
+    "url": "/receptionapp/static/media/personalinfotitle.085d4793.png"
+  },
+  {
+    "revision": "540a50f311b55ee93452f186fafac55c",
+    "url": "/receptionapp/static/media/stayoptionicon.540a50f3.png"
+  },
+  {
+    "revision": "d2f2fdbc13dc67be4a31dcfe1ff8460f",
+    "url": "/receptionapp/static/media/stayoptionselicon.d2f2fdbc.png"
+  },
+  {
+    "revision": "6acac7729fc1ca71221fc28564e6a7f7",
+    "url": "/receptionapp/static/media/stayoptiontitle.6acac772.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/receptionapp/precache-manifest.de264283154e2203d7cc33847e248a3d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/receptionapp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
